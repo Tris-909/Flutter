7c5a36bd53ab0769d182e6166c8bbf59f5394a37
Tri Tran <tranminhtri9090@gmail.com>
2023-03-09 16:08:58 +0700

    (4) Connecting button with function in Flutter

--- flutter_basics/lib/main.dart
+++ flutter_basics/lib/main.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("pressed");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +17,8 @@ class MyApp extends StatelessWidget {
         body: Column(children: [
           Text('text 1'),
           Text('text 2'),
-          ElevatedButton(onPressed: null, child: Text("Click me baby")),
+          ElevatedButton(
+              onPressed: answerQuestion, child: Text("Click me baby")),
         ]),
       ),
     );
