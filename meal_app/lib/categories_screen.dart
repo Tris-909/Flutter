c7236e73267202bc785c89c4a72fe3446c715ab7
Tri Tran <tranminhtri9090@gmail.com>
2023-04-06 20:11:50 +0700

    (39) Adding CategoryItem and CategoryScreen

--- meal_app/lib/categories_screen.dart
+++ meal_app/lib/categories_screen.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './cateogory_item.dart';
 
 class CatergoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
-    return GridView(
-      children: <Widget>[],
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(title: Text("MealApp")),
+        body: GridView(
+          children: DUMMY_CATEGORIES
+              .map((category) =>
+                  CategoryItem(title: category.title, color: category.color))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+        ));
   }
 }
