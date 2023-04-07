16308a25e2984aa975541972ff52c77553873f17
Tri Tran <tranminhtri9090@gmail.com>
2023-04-07 11:06:56 +0700

    (45) Improve MealItem widget

--- meal_app/lib/widgets/meal_item.dart
+++ meal_app/lib/widgets/meal_item.dart
@@ -17,6 +17,32 @@ class MealItem extends StatelessWidget {
     @required this.affordability,
   });
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+      case Complexity.Hard:
+        return 'Hard';
+      case Complexity.Challenging:
+        return 'Challenging';
+      default:
+        return "Undefined";
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Pricey:
+        return 'Pricey';
+      case Affordability.Luxurious:
+        return 'Luxurious';
+      default:
+        return "Undefined";
+    }
+  }
+
   void openDetailMealPage() {}
 
   @override
@@ -46,10 +72,59 @@ class MealItem extends StatelessWidget {
                   ),
                 ),
                 new Positioned(
-                  right: 100,
-                  child: Text(title),
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 320,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('${duration} minutes')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('${complexityText}')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('${affordabilityText}')
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
