866a0ba7595af7bb401909f29db4167d988372d6
Tri Tran <tranminhtri9090@gmail.com>
2023-04-22 12:07:11 +0700

    (57) Adding extra filter into Provider Product and use that in Product DetailScreen

--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    // final productId = ModalRoute.of(context).settings.arguments;
+    final productId = ModalRoute.of(context).settings.arguments;
+    final singleProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
 
     return Scaffold(
-      appBar: AppBar(title: Text("TEST")),
+      appBar: AppBar(title: Text(singleProduct.title)),
     );
   }
 }
