16a153332083b855216653ea0aae46dc25290568
Tri Tran <tranminhtri9090@gmail.com>
2023-04-22 11:23:19 +0700

    (56) Adding theme, ProductDetailScreen, ProductGrid and state management

--- shop_app/lib/widgets/productItem.dart
+++ shop_app/lib/widgets/productItem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
@@ -9,24 +10,33 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: null,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(ProductDetailScreen.routeName, arguments: id);
+          },
         ),
-        trailing: IconButton(
-          icon: Icon(Icons.shopping_cart),
-          onPressed: null,
-        ),
-        title: Text(
-          title,
-          textAlign: TextAlign.center,
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: null,
+          ),
+          trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: null,
+              color: Theme.of(context).accentColor),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
